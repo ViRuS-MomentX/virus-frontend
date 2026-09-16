ffc84f7a876c1e73833d7f2845c0dc5293a5f879
ViRuS-MomentX <xvladikcraftx@gmail.com>
2026-09-16 16:02:21 +0300

    Update posts-data.js

--- frontend/posts-data.js
+++ frontend/posts-data.js
@@ -2,84 +2,74 @@
 // его импортируют и лента, и главная страница.
 export const posts = [
   {
-    title: "Проверка фото",
+    title: "Первый пост",
     date: "2026-06-30",
     time: "14:31",
-    text: "Это мой попугайчик, а так же тест на фото в интерактиве",
-    image: "images/шерри.jpg",
-    category: "personal"
+    text: "https://discord.gg/bNdxCWZy3H",
+    image: "images/шерри.jpg"
   },
   {
     title: "Новая вкладка",
     date: "2026-06-29",
     time: "20:25",
     text: "Добавлена вкладка интерактив",
-    image: "",
-    category: "personal"
+    image: ""
   },
   {
     title: "Оао",
     date: "2026-06-29",
     time: "20:28",
     text: "ОНО РАБОТАЕТ!",
-    image: "",
-    category: "personal"
+    image: ""
   },
   {
     title: "Чистка",
     date: "2026-07-02",
     time: "22:22",
     text: "Сделал масштабную «уборку» в репозитории, теперь не стыдно оставлять ссылку на гитхаб",
-    image: "images/гитхаб.webp",
-    category: "personal"
+    image: "images/гитхаб.webp"
   },
   {
     title: "День Независимости",
     date: "2026-07-04",
     time: "18:16",
     text: "Happy independence day",
-    image: "images/сша.jpg",
-    category: "personal"
+    image: "images/сша.jpg"
   },
   {
     title: "Squad World",
     date: "2026-07-04",
     time: "20:49",
     text: "Добавлен новый проект - Squad World",
-    image: "images/squadworld.webp",
-    category: "game"
+    image: "images/squadworld.webp"
   },
   {
     title: "Новая иконка сайта",
     date: "2026-07-06",
     time: "00:10",
     text: "Добавлена новая иконка для сайта, я иконки хуже не видел, но за то она теперь есть",
-    image: "images/иконка.jpg",
-    category: "personal"
+    image: "images/иконка.jpg"
   },
   {
     title: "Эксперимент не был провальным",
     date: "2026-07-22",
     time: "16:50",
     text: "Ну имба же",
-    image: "images/voice.jpg",
-    category: "ai"
+    image: "images/voice.jpg"
   },
   {
     title: " ",
     date: "2026-07-22",
     time: "18:30",
     text: "На кристаликсе больше не играем",
-    image: "images/троян.jpg",
-    category: "game"
+    image: "images/троян.jpg"
   },
   {
     title: "Планы на будущее",
     date: "2026-07-31",
     time: "23:14",
     text: "Когда нибудь я доделаю этот сайт (завтра)",
-    image: "images/фонтан.jpg",
-    category: "personal"
+    image: "images/фонтан.jpg"
   },
   {
     title: "Обнова",
